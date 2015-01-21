b084d0f14f65b1ef5264bc6c4c4a33665078c72f
Clara Nees <cnees@umich.edu>
2015-01-20 22:17:52 -0500

    Enemies move around randomly and can be killed

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,26 +2,52 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	
 	public int health = 1;
-	public int damage; // Amount of damage to deal
-
+	public int damage = 1; // Amount of damage to deal
+	public Vector3 trajectory = Vector3.zero;
+	private int frames = 51; 
+	public float pixel = .0625f;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if(health < 1)
-			Destroy(this.gameObject);
+	void FixedUpdate () {
+		frames++;
+		if(frames < 20) {
+			transform.Translate (trajectory*pixel);
+		}
+		else {
+			// Pick a random direction
+			trajectory =  new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+			trajectory.Normalize ();
+			frames = 0;
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
 	}
 
 	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "Wall" || other.gameObject.tag == "Obstacle") {
+			trajectory = -1 * trajectory;
+			transform.Translate (pixel * trajectory);
+		}
 		
 		if (other.gameObject.tag == "Sword") {
-			print ("hit");
-			health--;
+			health -= 1;
+		}
+		else if (other.gameObject.tag == "Boomerang") {
+			health -= 1;
+		}
+		else if (other.gameObject.tag == "Bomb") {
+			health -= 4;
+		}
+		if(health <= 0) {
+			Destroy(gameObject);
+			print ("Enemy destroyed");
 		}
 	}
 }
