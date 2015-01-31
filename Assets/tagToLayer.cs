ebf4d48651dbc178f7a6b82f74499c05f6aab18f
Clara Nees <cnees@umich.edu>
2015-01-31 18:01:24 -0500

    Script for putting obstacles and walls in obstacle layer

---
+++ Assets/tagToLayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class tagToLayer : MonoBehaviour {
+
+	void Update () {
+		GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+		foreach (GameObject obstacle in obstacles) {
+			obstacle.transform.parent = null;
+		}
+		obstacles = GameObject.FindGameObjectsWithTag("Wall");
+		foreach (GameObject obstacle in obstacles) {
+			obstacle.transform.parent = null;
+		}
+	}
+
+}
